e1c53f0c14bfb8cfa8b2e83106e55f9547028079
Walker Pollitt <walker.pollitt@walkerpollittllc.com>
2026-07-12 14:42:30 -0400

    Remove conflicting Cloudflare Pages redirect rule

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   server: {
     port: 3000,
     open: true
